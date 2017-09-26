4e2b083b80569a3cf3f747d14a849a89628fe4ae
Vaclav Tolar <vaclav.tolar@gmail.com>
2017-09-26 22:50:19 +0200

    Development

--- app/actors/ScodashActor.scala
+++ app/actors/ScodashActor.scala
@@ -1,82 +1,41 @@
 package actors
 
-import akka.pattern.pipe
-import akka.persistence.{PersistentActor, SnapshotOffer}
-import common.PersistentEntity
+import java.util.UUID
 
-import scala.concurrent.{Future, Promise}
+import actors.Dashboard.Command.CreateDashboard
+import actors.Scodash.{CreateNewDashboard, FindDashboard}
+import common.Aggregate
+import common.PersistentEntity.GetState
+import org.apache.commons.lang3.RandomStringUtils
 
-class ScodashActor(id: String) extends PersistentEntity[DashboardFO](id) {
+import scala.collection.mutable
 
-  import ScodashActor._
-  import context._
-
-  private var state = ScodashState()
-
-  override def receiveRecover: Receive = {
-    case event: ScodashEvent =>
-      state += event
-    case SnapshotOffer(_, snapshot: ScodashState) =>
-      state = snapshot
-  }
-
-  override def receiveCommand: Receive = {
-    case GetDashboardActor(id) =>
-      val dashboardActorName = s"dashboard-${id.writeHash}"
-      context.child(dashboardActorName).getOrElse{
-        //context.actorOf(Props(id.writeHash), dashboardActorName))
-      }
-
-    case CreateDashboard(dashboard) =>
-      val dashboardActorName = s"dashboard-${dashboard.writeHash}"
-      val dashboardActor = context.actorOf(Dashboard.props(dashboard), dashboardActorName)
-      handleEvent(DashboardCreated(DashboardId(dashboard.writeHash), dashboardActorName)) pipeTo sender
-      ()
-  }
-
-  private def handleEvent[E <: ScodashEvent](e: => E): Future[E] = {
-    val p = Promise[E]
-    persist(e) { event =>
-      p.success(event)
-      state += event
-      context.system.eventStream.publish(event)
-      if (lastSequenceNr != 0 && lastSequenceNr % 1000 == 0) saveSnapshot(state)
-    }
-    p.future
-  }
-
-
-
-  override def persistenceId: String = "scodash"
-
-
+object Scodash {
+  case class FindDashboard(id: String)
+  case class CreateNewDashboard(name: String, description: String, style: String, items: mutable.Map[String, ItemFO] = mutable.Map(), ownerName: String, ownerEmail: String)
 }
 
-object ScodashActor {
+class Scodash extends Aggregate[DashboardFO, Dashboard] {
+  /**
+    * Gets the Props needed to create the child entity for this factory
+    *
+    * @return a Props instance
+    */
+  override def entityProps(id: String) = ???
 
-  sealed trait ScodashCommand
+  override def receive = {
+    case FindDashboard(id) =>
+      log.info("Finding dashboard {}", id)
+      val dashboard = lookupOrCreateChild(id)
+      forwardCommand(id, GetState)
 
-  case class CreateDashboard(dashboard: DashboardFO) extends ScodashCommand
-  case class GetDashboardActor(id: DashboardId) extends ScodashCommand
-
-  sealed trait ScodashEvent {
-    val id: DashboardId
-    val dashboardActorName: String
-  }
-
-  case class DashboardCreated(id: DashboardId, dashboardActorName: String) extends ScodashEvent
-  final case class DashboardNotFound(id: DashboardId) extends RuntimeException(s"Blog post not found with id $id")
-
-  type MaybeDashbord[+A] = Either[DashboardNotFound, A]
-
-  final case class ScodashState(dashboardActorNames: Map[DashboardId, String]) {
-    //def apply(id: DashboardId): MaybeDashbord[ActorRef] = dashboardActors.get(id).toRight(DashboardNotFound(id))
-    def apply(id: DashboardId): String = dashboardActorNames.get(id).get
-    def +(event: ScodashEvent): ScodashState = ScodashState(dashboardActorNames.updated(event.id, event.dashboardActorName))
-  }
-
-  object ScodashState {
-    def apply(): ScodashState = ScodashState(Map.empty)
+    case CreateNewDashboard(name, description, style, items, ownerName, ownerEmail) =>
+      log.info("Creating new dashboard with name {}", name)
+      val id = UUID.randomUUID().toString
+      val readonlyHash = RandomStringUtils.randomAlphanumeric(8)
+      val writeHash = RandomStringUtils.randomAlphanumeric(8)
+      val fo = DashboardFO(id, name, description, style, items, ownerName, ownerEmail, readonlyHash, writeHash)
+      val command = CreateDashboard(fo)
+      forwardCommand(id, command)
   }
 }
-
