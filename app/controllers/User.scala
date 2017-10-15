b4cdcf3e7ddb8c3c61f6c1aca1bf3a6f208d4ad2
Vaclav Tolar <vaclav.tolar@gmail.com>
2017-10-15 22:10:51 +0200

    Development

--- app/controllers/User.scala
+++ app/controllers/User.scala
@@ -1,16 +1,29 @@
 package controllers
 
-import akka.actor.Props
+import akka.actor.{ActorRef, Props}
+import com.fasterxml.jackson.databind.node.ObjectNode
+import play.libs.Json
 
 case class UserFO(id: String)
 
-class User(id: String) extends AbstractBaseActor {
-  override def receive = ???
+class User(id: String, out: ActorRef) extends AbstractBaseActor {
+  override def receive = {
+    case addItem: Dashboard.Command.AddItem =>
+      val message = Json.newObject.put("type", "additem").put("name", addItem.name)
+      out ! message
+    case removeItem: Dashboard.Command.RemoveItem =>
+      val message = Json.newObject.put("type", "removeitem").put("name", removeItem.name)
+      out ! message
+    case cmd:Any =>
+      log.warning("Unexpected user command {}", cmd)
+
+
+  }
 }
 
 object User {
   val Name = "user"
-  def props(id: String) = Props(classOf[User], id)
+  def props(id: String, out: ActorRef) = Props(classOf[User], id, out)
 
 }
 
