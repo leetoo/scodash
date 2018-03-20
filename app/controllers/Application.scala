fba3569002df2a9beeec6c657bb5c192924001ce
Vaclav Tolar <vaclav.tolar@gmail.com>
2018-03-20 22:19:33 +0100

    Development

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -236,7 +236,8 @@ class Application @Inject()(
     * Returns true if the value of the Origin header contains an acceptable value.
     */
   def originMatches(origin: String): Boolean = {
-    origin.contains("localhost:9000") || origin.contains("localhost:19001")
+    val host = sys.env("HOST")
+    origin.contains(host + ":9000") || origin.contains(host + ":19001")
   }
 
   /**
