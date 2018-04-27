5fa45f41e7292b60d1691ef53c71e4f8333ce586
e_vtolar <vaclav_tolar@kb.cz>
2018-04-27 17:20:27 +0200

    Json refactoring

--- app/controllers/JsonSupport.scala
+++ app/controllers/JsonSupport.scala
@@ -2,8 +2,8 @@ package controllers
 
 import org.joda.time.DateTime
 import org.joda.time.format.DateTimeFormat
-import play.api.libs.json._
 import play.api.libs.functional.syntax._
+import play.api.libs.json._
 
 trait JsonSupport {
 
@@ -23,11 +23,61 @@ trait JsonSupport {
   implicit val newDashboardReads: Reads[Forms.NewDashboard] = (
     (JsPath \ "name").read[String] and
       (JsPath \ "description").read[String]
-    )(Forms.NewDashboard.apply _)
+    ) (Forms.NewDashboard.apply _)
+
+  implicit val createDashboardItemsReads: Reads[Forms.CreateDashboardItems] =
+    (JsPath \ "items").read[Set[String]]
+  Forms.CreateDashboardItems.apply _
+
+  implicit val createDashboardReads: Reads[Forms.CreatedDashboard] =
+    (JsPath \ "name").read[String] and
+    (JsPath \ "writeHash").read[String] and
+    (JsPath \ "readOnlyHash").read[String]
+  Forms.CreatedDashboard.apply _
+
+  implicit val dashboardReads: Reads[Forms.Dashboard] =
+    (JsPath \ "name").read[String] and
+      (JsPath \ "description").read[String] and
+      (JsPath \ "items").read[Set[String]] and
+      (JsPath \ "ownerName").read[String] and
+      (JsPath \ "ownerEmail").read[String]
+  Forms.Dashboard.apply _
+
+  implicit val dashboardWrites: Writes[Forms.Dashboard] =
+    (JsPath \ "name").write[String] and
+      (JsPath \ "description").write[String] and
+      (JsPath \ "items").write[Set[String]] and
+      (JsPath \ "ownerName").write[String] and
+      (JsPath \ "ownerEmail").write[String]
+  unlift(Forms.Dashboard.unapply)
+
+  // FO classes
+  implicit val dashboardFoWrites: Writes[DashboardFO] =
+    (JsPath \ "id").write[String] and
+      (JsPath \ "name").write[String] and
+      (JsPath \ "description").write[String] and
+      (JsPath \ "items").write[Set[String]] and
+      (JsPath \ "ownerName").write[String] and
+      (JsPath \ "ownerEmail").write[String] and
+      (JsPath \ "readonlyHash").write[String] and
+      (JsPath \ "writeHash").write[String] and
+      (JsPath \ "created").write[DateTime] and
+      (JsPath \ "deleted").write[DateTime]
+  unlift(Forms.DashboardFo.unapply)
+
+  implicit val dashboardFoReads: Reads[DashboardFO] =
+    (JsPath \ "id").write[String] and
+      (JsPath \ "name").write[String] and
+      (JsPath \ "description").write[String] and
+      (JsPath \ "items").write[Set[String]] and
+      (JsPath \ "ownerName").write[String] and
+      (JsPath \ "ownerEmail").write[String] and
+      (JsPath \ "readonlyHash").write[String] and
+      (JsPath \ "writeHash").write[String] and
+      (JsPath \ "created").write[DateTime] and
+      (JsPath \ "deleted").write[DateTime]
+  DashboardFo.apply _
 
 
 
-  implicit val residentReads: Reads[Forms.CreateDashboardItems] = (
-    (JsPath \ "items") read[Set[String]]
-    )(Forms.CreateDashboardItems.apply _)
 }
